5bc260695a94906e4dad8476d7f458dc744ad8d1
sitaramshelke <sitaramshelke@gmail.com>
2016-06-11 00:50:55 +0530

    Add code to cache metrics

--- pcp-pidstat.py
+++ pcp-pidstat.py
@@ -10,29 +10,39 @@ PIDSTAT_METRICS = ['pmda.uname','hinv.ncpu','proc.psinfo.pid','proc.nprocs','pro
 class ReportingMetricRepository:
     def __init__(self,group):
         self.group = group
+        self.current_cached_values = {}
+        self.previous_cached_values = {}
 
     def current_value(self, metric, instance):
         if not metric in self.group:
             return None
         if instance:
-            lst = dict(map(lambda x: (x[0].inst, x[2]), self.group[metric].netValues))
-            if instance in lst.keys():
-                return lst.get(instance,None)
+            if not metric in self.current_cached_values.keys():
+                lst = dict(map(lambda x: (x[0].inst, x[2]), self.group[metric].netValues))
+                self.current_cached_values[metric] = lst
+            if instance in self.current_cached_values[metric].keys():
+                return self.current_cached_values[metric].get(instance,None)
             else:
                 return None
         else:
-            return self.group[metric].netValues[0][2]
+            if not metric in self.current_cached_values.keys():
+                self.current_cached_values[metric] = self.group[metric].netValues[0][2]
+            return self.current_cached_values[metric]
     def previous_value(self, metric, instance):
         if not metric in self.group:
             return None
         if instance:
-            lst = dict(map(lambda x: (x[0].inst, x[2]), self.group[metric].netPrevValues))
-            if instance in lst.keys():
-                return lst.get(instance,None)
+            if not metric in self.current_cached_values.keys():
+                lst = dict(map(lambda x: (x[0].inst, x[2]), self.group[metric].netPrevValues))
+                self.current_cached_values[metric] = lst
+            if instance in self.current_cached_values[metric].keys():
+                return self.current_cached_values[metric].get(instance,None)
             else:
                 return None
         else:
-            return self.group[metric].netPrevValues[0][2]
+            if not metric in self.current_cached_values.keys():
+                self.current_cached_values[metric] = self.group[metric].netPrevValues[0][2]
+            return self.current_cached_values[metric]
 class UserCpuUsage:
     def __init__(self, group):
         self.group = group
