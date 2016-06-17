b393efd400b374f94cd14940247db930fef2aabc
sitaramshelke <sitaramshelke@gmail.com>
2016-06-17 12:43:05 +0530

    Fix total percent time test

--- test/cpu_usage_test.py
+++ test/cpu_usage_test.py
@@ -1,5 +1,6 @@
 import unittest
 from mock import Mock
+import  mock
 from pcp_pidstat import CpuUsage
 
 
@@ -37,17 +38,58 @@ class TestUserCpuUsage(unittest.TestCase):
 
         self.assertEquals(system_percent_used, 10.0)
 
+    def c_repo_side_effect(self, metric_name,instance):
+        if metric_name == 'proc.psinfo.utime' and instance == 1:
+            return 112233
+        if metric_name == 'proc.psinfo.guest_time' and instance == 1:
+            return 112213
+        if metric_name == 'proc.psinfo.stime' and instance == 1:
+            return 112243
+        if metric_name == 'proc.psinfo.pid' and instance == 1:
+            return 1
+        if metric_name == 'proc.psinfo.cmd' and instance == 1:
+            return "test"
+        if metric_name == 'proc.psinfo.processor' and instance == 1:
+            return 0
+        if metric_name == 'proc.id.uid' and instance == 1:
+            return 1
+        if metric_name == 'proc.id.uid_nm' and instance == 1:
+            return "ram"
 
-    # def test_cpu_usage_system_time_percentage_for_instance(self):
-    #     m_repo_mock = Mock()
-    #     cpu_usage = CpuUsage(m_repo_mock)
-    #     cpu_usage.user_for_instance = Mock(return_value=2.23)
-    #     cpu_usage.guest_for_instance = Mock(return_value=0.00)
-    #     cpu_usage.system_for_instance = Mock(return_value=4.12)
-    #
-    #     cpu_percent_used = cpu_usage.cpuusage_for_instance(123, 1.34)
-    #
-    #     self.assertEquals(cpu_percent_used, 6.35)
+    def p_repo_side_effect(self, metric_name,instance):
+        if metric_name == 'proc.psinfo.utime' and instance == 1:
+            return 112223
+        if metric_name == 'proc.psinfo.guest_time' and instance == 1:
+            return 112203
+        if metric_name == 'proc.psinfo.stime' and instance == 1:
+            return 112233
+        if metric_name == 'proc.psinfo.pid' and instance == 1:
+            return 1
+        if metric_name == 'proc.psinfo.cmd' and instance == 1:
+            return "test"
+        if metric_name == 'proc.psinfo.processor' and instance == 1:
+            return 0
+        if metric_name == 'proc.id.uid' and instance == 1:
+            return 1
+        if metric_name == 'proc.id.uid_nm' and instance == 1:
+            return "ram"
+
+    def test_cpu_usage_total_time_percentage_for_instance(self):
+        m_repo_mock = Mock()
+
+        cpu_usage = CpuUsage(m_repo_mock)
+        with mock.patch.object(cpu_usage,'_CpuUsage__pids',return_value=[1]) as method:
+            m_repo_mock.current_value = Mock(side_effect=self.c_repo_side_effect)
+            m_repo_mock.previous_value = Mock(side_effect=self.p_repo_side_effect)
+            test_user_percent = float("%.2f"%(100*(112233 -112223)/(1.34*1000)))
+            test_guest_percent = float("%.2f"%(100*(112213-112203)/(1.34*1000)))
+            test_system_percent = float("%.2f"%(100*(112243-112233)/(1.34*1000)))
+
+            test_total_percent = test_user_percent+test_guest_percent+test_system_percent
+            process_info = cpu_usage.get_processes(1.34)
+            total_percent = process_info[0].total_percent
+
+            self.assertEquals(total_percent, test_total_percent)
 
 if __name__ == '__main__':
     unittest.main()
